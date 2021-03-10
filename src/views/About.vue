9448289fe1f692b2771365baa1157c0595938b0c
Maxime Moreillon <m.moreillon@gmail.com>
2021-03-10 05:18:40 +0000

    manual

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,7 +14,7 @@
       Back-end version: {{back_end_version}}
     </p>
     <p>
-      Desktop client manual: <a href="行先掲示板_desktop_client_manual.pdf.pdf">download</a>
+      Desktop client manual: <a href="行先掲示板_desktop_client_manual.pdf">download</a>
     </p>
 
 
