53d50ddafdc34ee36768e5bb050ad56ee143aa85
Maxime Moreillon <m.moreillon@gmail.com>
2021-12-14 08:13:41 +0900

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+
     <div class="action_bar">
       <router-link :to="{ name: 'groups'}">
         <ArrowLeftIcon />
@@ -7,7 +8,6 @@
       </router-link>
 
       <div class="spacer"/>
-
     </div>
 
     <h1>行先掲示板</h1>
@@ -17,15 +17,26 @@
     <p>
       Developped and maintained by <a href="https://maximemoreillon.com">Maxime MOREILLON</a>
     </p>
-    <p>
-       Front end version: {{version}}
-    </p>
-    <p>
-      Back-end version: {{back_end_version}}
-    </p>
+
     <p>
       Desktop client manual: <a href="行先掲示板_desktop_client_manual.pdf">download</a>
     </p>
+
+    <h2>Services</h2>
+    <table>
+      <tr>
+        <th>Service</th>
+        <th>Version</th>
+        <th>URL</th>
+      </tr>
+      <tr
+        v-for="(service, index) in services"
+        :key="`service_${index}`">
+        <td>{{service.name}}</td>
+        <td>{{service.version}}</td>
+        <td>{{service.url}}</td>
+      </tr>
+    </table>
 
 
 
@@ -46,31 +57,75 @@ export default {
   },
   data () {
     return {
-      application_count: null,
-      back_end_version: null,
-      version: pjson.version,
+
+      services: [
+        {
+          name: 'Whereabouts GUI',
+          url: window.location.origin,
+          version: pjson.version
+        },
+        {
+          name: 'Whereabouts API',
+          url: process.env.VUE_APP_WHEREABOUTS_API_URL,
+          version: null
+        },
+        {
+          name: 'Authentication API',
+          url: process.env.VUE_APP_AUTHENTICATION_API_URL,
+          version: null
+        },
+        {
+          name: 'Group manager API',
+          url: process.env.VUE_APP_GROUP_MANAGER_API_URL,
+          version: null
+        },
+        {
+          name: 'Employee manager API',
+          url: process.env.VUE_APP_EMPLOYEE_MANAGER_API_URL,
+          version: null
+        },
+
+      ],
 
     }
   },
-  mounted(){
-    this.get_back_end_version()
+  mounted () {
+    this.get_services_version()
   },
   methods: {
 
-    get_back_end_version(){
-      let url = `${process.env.VUE_APP_WHEREABOUTS_API_URL}/`
-      this.axios.get(url)
-      .then((response) => {
-        this.back_end_version = response.data.version
+    get_services_version () {
+      this.services.forEach((service) => {
+        if (service.version) return
+        service.version = 'Connecting...'
+        this.axios.get(service.url)
+          .then(({ data }) => { service.version = data.version })
+          .catch(() => { service.version = 'Unable to connect' })
       })
-      .catch(error => console.log(error))
     }
   }
+
 
 }
 </script>
 
 <style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  table-layout: fixed;
 
+}
+
+tr:not(:last-child) {
+  border-bottom: 1px solid #dddddd;
+}
+
+th {
+  text-align: left;
+}
+td {
+  padding: 0.25em;
+}
 
 </style>
