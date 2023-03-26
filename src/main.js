832ca00d1d91a469300648ac33faa8f53e6f848d
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-27 08:01:28 +0900

    debugging env

--- src/main.js
+++ src/main.js
@@ -1,49 +1,47 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import VueCookies from 'vue-cookies'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import VueSocketIOExt from 'vue-socket.io-extended'
-import io from 'socket.io-client'
-import Loader from '@moreillon/vue_loader'
-import vuetify from './plugins/vuetify'
-import i18n from './i18n'
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueCookies from "vue-cookies";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import VueSocketIOExt from "vue-socket.io-extended";
+import io from "socket.io-client";
+import Loader from "@moreillon/vue_loader";
+import vuetify from "./plugins/vuetify";
+import i18n from "./i18n";
+import "./registerServiceWorker";
 
+VueCookies.config("100d");
 
-VueCookies.config('100d')
+const { VUE_APP_WHEREABOUTS_API_URL } = process.env;
+console.log(VUE_APP_WHEREABOUTS_API_URL);
 
-const socket = io(`${process.env.VUE_APP_WHEREABOUTS_API_URL}`)
+const socket = io(VUE_APP_WHEREABOUTS_API_URL);
 
-Vue.use(VueSocketIOExt, socket)
+Vue.use(VueSocketIOExt, socket);
 
-Vue.use(VueCookies)
-Vue.use(VueAxios, axios)
-
+Vue.use(VueCookies);
+Vue.use(VueAxios, axios);
 
 // Registering components
-Vue.component('Loader', Loader)
+Vue.component("Loader", Loader);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+socket.on("connect", () => {});
 
-socket.on('connect', () => {
-})
-
-socket.on('disconnect', () => {
-  store.commit('set_connected',false)
-})
-socket.on('authenticated', () => {
-  store.commit('set_connected',true)
-})
-
+socket.on("disconnect", () => {
+  store.commit("set_connected", false);
+});
+socket.on("authenticated", () => {
+  store.commit("set_connected", true);
+});
 
 new Vue({
   store,
   router,
   vuetify,
   i18n,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
